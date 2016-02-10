ebac6c92c99c57cc312d10ba7f7e0295d9137b47
ADI <pricope.adrian.fit@gmail.com>
2016-02-10 22:38:07 +0200

    servlet for db read to html

--- src/main/Java/DbServlet.java
+++ src/main/Java/DbServlet.java
@@ -9,22 +9,26 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-@WebServlet("/pacienti")
-public class PacientiServlet extends HttpServlet {
-
-    @Resource("jdbc/YourDB") // For Tomcat, define as <Resource> in context.xml and declare as <resource-ref> in web.xml.
+//@WebServlet("/pacienti")
+public class DbServlet extends HttpServlet {
+    //@Resource("jdbc/Agenda_Adrian") // For Tomcat, define as <Resource> in context.xml and declare as <resource-ref> in web.xml.
     private DataSource dataSource;
-    private DatabaseRead databaseRead;
+    public DatabaseRead databaseRead;
 
     @Override
-    public void init() {
-        databaseRead = new DatabaseRead(dataSource);
-    }
+   public void init() {
+       databaseRead = new DatabaseRead(dataSource);
+   }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            List<Pacient> pacienti = databaseRead.list();
+            List<Pacient> pacienti = null;
+            try {
+                pacienti = DatabaseRead.list();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
             request.setAttribute("pacienti", pacienti); // Will be available as ${products} in JSP
             request.getRequestDispatcher("/WEB-INF/products.jsp").forward(request, response);
         } catch (SQLException e) {
